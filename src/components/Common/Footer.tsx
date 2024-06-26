b6b072247f5bf57ef347b5021247cd05ec15b89b
Damso74 <122619191+Damso74@users.noreply.github.com>
2024-06-26 12:10:07 +0200

    Update Footer.tsx

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,4 +1,5 @@
-// src/components/Common/Footer.jsx
+// src/components/Common/Footer.tsx
+import { Link } from 'react-router-dom';
 import './Footer.css'; // Assurez-vous d'ajouter les styles correspondants
 
 function Footer() {
@@ -6,12 +7,12 @@ function Footer() {
     <footer className="footer">
       <div className="container">
         <section className="footer-links">
-          <a href="/about">About</a>
-          <a href="/docs">Documentation</a>
-          <a href="/contact">Contact</a>
+          <Link to="/about">About</Link>
+          <Link to="/docs">Documentation</Link>
+          <Link to="/contact">Contact</Link>
         </section>
         <section className="footer-open-source">
-          <p>This project is open source. Contribute on <a href="https://github.com/Damso74/AuthChain-Frontend-V1.git">GitHub</a>.</p>
+          <p>This project is open source. Contribute on <a href="https://github.com/Damso74/AuthChain-Frontend-V1.git" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
         </section>
       </div>
     </footer>
